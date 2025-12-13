736107d73003313342316a25344945bdb14a68e3
avsolorz <victoriasolorzano62@gmail.com>
2025-12-13 15:15:16 -0500

    Creacion de Aspirante

---
+++ src/aspirante/dto/create-aspirante.dto.ts
@@ -0,0 +1,27 @@
+import { IsString, IsOptional, Length, IsDateString } from 'class-validator';
+
+export class CreateAspiranteDto {
+
+  @IsString()
+  id_contacto: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(1, 50)
+  nacionalidad?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(1, 1)
+  genero?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(1, 30)
+  estado_civil?: string;
+
+  @IsOptional()
+  @IsDateString()
+  fecha_nacimiento?: Date;
+
+}
